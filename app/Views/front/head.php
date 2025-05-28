3af0b7734b575f81b7eb7364a96f72b204a2ce24
MatiasSolisSchneeberger <matias.solis.sch@gmail.com>
2025-05-28 00:03:24 -0300

    Update head.php

--- app/Views/front/head.php
+++ app/Views/front/head.php
@@ -5,7 +5,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Tienda Libre</title>
+	<link rel="icon" type="image/x-icon" href="assets/images/Icono.png">
 	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/mis-estilos.css" rel="stylesheet">
 </head>
 
 <body>
