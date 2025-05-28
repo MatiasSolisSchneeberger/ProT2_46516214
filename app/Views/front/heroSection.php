016b095e55504adf2aa96f22a82abf14819aa4b6
MatiasSolisSchneeberger <matias.solis.sch@gmail.com>
2025-05-28 00:03:31 -0300

    Update heroSection.php

--- app/Views/front/heroSection.php
+++ app/Views/front/heroSection.php
@@ -1,15 +1,15 @@
 <!-- Hero Section -->
-<div class="px-4 py-5 my-5 text-center">
+<section class="text-center text-white heroSection">
     <!-- icono -->
     <img class="d-block mx-auto mb-4" src="assets/images/Icono.png" alt="" width="40" height="40">
 
-    <h1 class="display-7 fw-bold">Compra lo que amas, sin complicaciones.</h1>
+    <h1 class="display-7 fw-bold ">Compra lo que amas, sin complicaciones.</h1>
 
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">Descubre productos únicos, precios increíbles y envíos rápidos. Tu nueva tienda online favorita está aquí.</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Ver productos</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Conocenos</button>
+            <button type="button" class="btn btn-secondary btn-lg px-4">Conocenos</button>
         </div>
     </div>
-</div>
+</section>
