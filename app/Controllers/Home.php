79dee2f0d247035d3d59c846b7ef984a9aa9508b
MatiasSolisSchneeberger <matias.solis.sch@gmail.com>
2025-05-31 10:09:48 -0300

    agregando nuevas paginas

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,10 +6,59 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo view('front/head');
-        echo view('front/navbar');
-        echo view('front/heroSection');
-        echo view('front/productos');
-        echo view('front/footer');
+        echo view('front/componentes/head');
+        echo view('front/secciones/navbar');
+        echo view('front/secciones/heroSection');
+        echo view('front/secciones/productos');
+        echo view('front/secciones/footer');
     }
+    
+    public function productos()
+    {
+        echo view('front/componentes/head');
+        echo view('front/secciones/navbar');
+        echo view('front/componentes/productosTodos');
+        echo view('front/secciones/footer');
+    }
+
+    public function iPhone16ProMax()
+    {
+        echo view('front/componentes/head');
+        echo view('front/secciones/navbar');
+        echo view('front/secciones/productoPresentacion');
+        echo view('front/secciones/Q&A');
+        echo view('front/secciones/productos');
+        echo view('front/secciones/footer');
+    }
+
+    public function sobreNosotros()
+    {
+        echo view('front/componentes/head');
+        echo view('front/secciones/navbar');
+        echo view('front/secciones/sobreNosotros');
+        echo view('front/secciones/footer');
+    }
+    public function AcercaDe()
+    {
+        echo view('front/componentes/head');
+        echo view('front/secciones/navbar');
+        echo view('front/secciones/AcercaDe');
+        echo view('front/secciones/footer');
+    }
+    public function Registrarse()
+    {
+        echo view('front/componentes/head');
+        echo view('front/secciones/navbar');
+        echo view('front/secciones/Registrarse');
+        echo view('front/secciones/footer');
+    }
+    public function IniciarSesion()
+    {
+        echo view('front/componentes/head');
+        echo view('front/secciones/navbar');
+        echo view('front/secciones/IniciarSesion');
+        echo view('front/secciones/footer');
+    }
+
+
 }
